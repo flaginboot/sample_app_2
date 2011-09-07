f6e9c619ab39277bcc8123eb37a3242256453188
Travis Wetmore <traviswetmore@gmail.com>
2011-09-06 21:26:35 -0400

    added help page

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+
   test "should get home" do
     get :home
     assert_response :success
@@ -8,7 +13,7 @@ class PagesControllerTest < ActionController::TestCase
 
   test 'home should have the right title' do
     get :home
-    assert_tag :title, content: "Ruby on Rails Tutorial Sample App | Home"
+    assert_tag :title, content: @base_title + " | Home"
   end
 
 
@@ -20,7 +25,7 @@ class PagesControllerTest < ActionController::TestCase
 
   test 'contact should have the right title' do
     get :contact
-    assert_tag :title, content: "Ruby on Rails Tutorial Sample App | Contact"
+    assert_tag :title, content: @base_title + " | Contact"
   end
 
 
@@ -32,7 +37,18 @@ class PagesControllerTest < ActionController::TestCase
 
   test 'about should have the right title' do
     get :about
-    assert_tag :title, content: "Ruby on Rails Tutorial Sample App | About"
+    assert_tag :title, content: @base_title + " | About"
+  end
+
+
+
+  test 'should get help' do
+    get :help
+    assert_response :success
+  end
+  test 'help should have the right title' do
+    get :help
+    assert_tag :title, content: @base_title + " | Help"
   end
 end
 
